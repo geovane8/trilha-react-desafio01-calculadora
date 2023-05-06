089c660a689ec73908bfdf56051d13a2f48ca824
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-05-06 15:23:52 -0300

    Update styles.js

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,29 +1,36 @@
 import styled from 'styled-components';
 
-export const InputContainer = styled.div`
-    width: 100%;
-    height: 75px;
-    background-color: #AAAAFF;
+// Defina as variáveis ​​de cor
+const primaryColor = '#AAAAFF';
 
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
+// Defina o container com uma classe mais descritiva
+export const InputWrapper = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: ${primaryColor};
 
-    font-size: 24px;
-    font-family: 'Roboto';
-    
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 
-    input {
-        width: 100%;
-        height: 75px;
-        background-color: #AAAAFF;
-        border: 0;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-end;
-        padding: 0 10px;
-        font-size: 24px;
-        font-family: 'Roboto';
-        color: #FFFFFF;
-    }
-`
+  font-size: 24px;
+  font-family: 'Roboto';
+`;
+
+// Defina o input com uma classe para ser reutilizado em outros lugares
+export const Input = styled.input`
+  width: 100%;
+  height: 75px;
+  border: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  padding: 0 10px;
+  font-size: 24px;
+  font-family: 'Roboto';
+
+  /* Remova as propriedades de cor aqui e mova-as para o InputWrapper */
+`;
+
+// Adicione um rótulo acessível para o input
+<Input aria-label="Digite aqui o seu texto" />
