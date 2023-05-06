1689ad240861b333b0696137fc3650dea8711ad3
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-05-06 15:19:39 -0300

    Update styles.js

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,15 +1,28 @@
 import styled from 'styled-components';
 
-export const ButtonContainer = styled.button`
-    padding: 20px;
-    border: 1px solid #CDCDCD;
-    background-color: #00AAF0;
-    color: #FFFFFF;
-    font-size: 24px;
-    font-weight: 700;
-    flex: 1;
-    
-    &:hover {
-        opacity: 0.6;
-    }
-`
+// Defina as variáveis ​​de cor
+const primaryColor = '#00AAF0';
+const borderColor = '#CDCDCD';
+const textColor = '#FFFFFF';
+
+// Defina o botão com uma classe para ser reutilizado em outros lugares
+export const Button = styled.button`
+  /* Adicione um valor padrão para o cursor */
+  cursor: pointer;
+
+  /* Adicione acessibilidade ao botão */
+  padding: 20px;
+  border: 1px solid ${borderColor};
+  background-color: ${primaryColor};
+  color: ${textColor};
+  font-size: 24px;
+  font-weight: 700;
+  flex: 1;
+
+  /* Adicione uma transição suave ao :hover */
+  transition: opacity 0.3s ease-in-out;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`;
