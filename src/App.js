20cb0e5cac59da15d84a9b760c1a6a0e81104ca6
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-05-06 15:42:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -121,12 +121,28 @@ const App = () => {
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="x" onClick={handleMultiplyNumbers}/>
-          <Button label="/" onClick={handleDivideNumbers}/>
-          <Button label="c" onClick={handleOnClear}/>
-          <Button label="." onClick={() => handleAddNumber('.')}/>
-        </Row>
-        <Row>
-          <Button label="7" onClick={() => handleAddNumber('7')}/>
-          <Button label="8" onClick={() => handleAddNumber('8')}/>
-          <Button label="9" onClick={()
+          <Row>
+              <Button label="x" onClick={() => handleMultiplyNumbers()} />
+              <Button label="/" onClick={() => handleDivideNumbers()} />
+              <Button label="c" onClick={handleOnClear} />
+              <Button label="%" onClick={() => handlePercentage()} />
+            </Row>
+            <Row>
+              <Button label="7" onClick={() => handleAddNumber('7')} />
+              <Button label="8" onClick={() => handleAddNumber('8')} />
+              <Button label="9" onClick={() => handleAddNumber('9')} />
+              <Button label="-" onClick={() => setOperation('-')} />
+            </Row>
+            <Row>
+              <Button label="4" onClick={() => handleAddNumber('4')} />
+              <Button label="5" onClick={() => handleAddNumber('5')} />
+              <Button label="6" onClick={() => handleAddNumber('6')} />
+              <Button label="+" onClick={() => setOperation('+')} />
+            </Row>
+            <Row>
+              <Button label="1" onClick={() => handleAddNumber('1')} />
+              <Button label="2" onClick={() => handleAddNumber('2')} />
+              <Button label="3" onClick={() => handleAddNumber('3')} />
+              <Button label="=" onClick={() => handleEquals()} />
+            </Row>
+
