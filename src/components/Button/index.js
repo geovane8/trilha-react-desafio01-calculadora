a6390474996c160819f66639a0e0fc7b19d02c05
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-05-06 15:20:55 -0300

    Update index.js

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,12 +1,17 @@
-
 import { ButtonContainer } from './styles';
 
-const Button = ({label, onClick}) => {
-    return (
-      <ButtonContainer onClick={onClick} type="button">
-       {label}
-      </ButtonContainer>
-    );
-  }
-  
-  export default Button;
+/**
+ * Um botão reutilizável que pode ser usado em diferentes partes do aplicativo.
+ * @param {string} label - O texto exibido no botão.
+ * @param {function} onClick - Uma função a ser executada quando o botão for clicado.
+ */
+const Button = ({ label, onClick }) => {
+  return (
+    <ButtonContainer onClick={onClick} type="button" aria-label={label}>
+      {/* Verifique se a prop label existe antes de renderizá-la */}
+      {label && label}
+    </ButtonContainer>
+  );
+};
+
+export default Button;
