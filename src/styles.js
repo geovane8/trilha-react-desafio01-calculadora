1d0e52aa3c4f21e383b25c4680fbb76e8e1d5cc6
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-05-06 15:29:35 -0300

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,32 +1,37 @@
 import styled from 'styled-components';
 
+// Container
+// Define o estilo do contêiner principal
 export const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: #CACACA;
+  width: 100%;
+  height: 100vh;
+  background-color: #CACACA;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-
-
+// Content
+// Define o estilo do conteúdo dentro do contêiner
 export const Content = styled.div`
-    background-color: #FFFFFF;
-    width: 50%;
-`
+  background-color: #FFFFFF;
+  width: 50%;
+`;
 
+// Row
+// Define um estilo para uma linha horizontal de itens
 export const Row = styled.div`
-    display:flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: inherit; // Herda a propriedade align-items do pai
+`;
 
+// Column
+// Define um estilo para uma coluna vertical de itens
 export const Column = styled.div`
-    display:flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-`
-
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: inherit; // Herda a propriedade align-items do pai
+`;
